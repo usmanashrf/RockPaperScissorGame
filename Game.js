3be091a2efdf2f41ea7ac7b426a309898419e88a
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2022-11-28 00:04:31 +0500

    create basic logic

---
+++ Game.js
@@ -0,0 +1,57 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+const options = ['Rock', 'Paper', 'Scissor'];
+let playAgain = true;
+let scores = 0;
+async function Game() {
+    while (playAgain) {
+        let computerPick = options[Math.floor(Math.random() * options.length)];
+        let answers = await inquirer.prompt([
+            {
+                type: 'list',
+                name: 'option',
+                message: '',
+                choices: [
+                    'Rock',
+                    'Paper',
+                    'Scissor'
+                ]
+            }
+        ]);
+        console.log(chalk.bgCyan(`Computer Picks ${computerPick}`));
+        if (options.indexOf(answers.option) === 0 && options.indexOf(computerPick) === 2) {
+            console.log(chalk.green("Congrats !!  +10 points"));
+            console.log(chalk.bgGreen(`${scores += 10}`));
+        }
+        else if (options.indexOf(answers.option) === 2 && options.indexOf(computerPick) === 0) {
+            console.log(chalk.red("You Lose"));
+            playAgain = await Repeat();
+        }
+        else if (options.indexOf(answers.option) > options.indexOf(computerPick)) {
+            console.log(chalk.green("Congrats !!  +10 points"));
+            console.log(chalk.bgGreen(`${scores += 10}`));
+        }
+        else if (options.indexOf(answers.option) == options.indexOf(computerPick)) {
+            console.log(chalk.yellow("Draw !!"));
+        }
+        else {
+            console.log(chalk.red("You Lose"));
+            playAgain = await Repeat();
+        }
+    }
+}
+async function Repeat() {
+    let Repeat = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'option',
+            message: 'Play Again ???',
+            choices: [
+                'Yes',
+                'No'
+            ]
+        }
+    ]);
+    return Repeat.option === 'Yes' ? true : false;
+}
+export default Game;
---
+++ index.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+import chalk from "chalk";
+import Game from "./Game.js";
+function banner() {
+    console.log(chalk.bgGreen("Rock"));
+    console.log(chalk.green(` 
+         ____.___
+    ---./   ._._\\)
+       |   / (_\\_\\)
+       |_ '  (___)
+     _  |    (___)
+    __-|'----'                        
+ `));
+    console.log(chalk.bgBlue("Paper"));
+    console.log(chalk.blue(`         
+         ___..__
+__..--""" ._ __.'
+           "-..__
+         '"--..__";
+___       '--...__"";
+ '-..__ '"---..._;"
+       """"----'  
+        `));
+    console.log(chalk.bgRed("Scissor"));
+    console.log(chalk.red(` 
+ .-.  _
+ | | / )
+ | |/ /
+_|__ /_
+/ __)-' )
+\\  '(.-')
+> ._>-'
+/ \\/`));
+    console.log(chalk.bgCyan(" Created By: "));
+    console.log(chalk.green("Usman Ashraf"));
+    setTimeout(() => {
+        console.log(chalk.green(""));
+    }, 10);
+}
+banner();
+Game();
